86cd6a9e253a98e403669ed3a36245769f966531
Gergely Kiss <gergely.kiss@r34dy.io>
2022-11-19 14:36:26 +0100

    mvp product page

--- pages/product/[productId].tsx
+++ pages/product/[productId].tsx
@@ -10,6 +10,7 @@ import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 import Star from "../../svg/Star";
 import EmptyStar from "../../svg/EmptyStar";
 import styles from "./productPage.module.scss";
+import Head from "next/head";
 
 const ProductPage: NextPage = () => {
   const router = useRouter();
@@ -37,61 +38,64 @@ const ProductPage: NextPage = () => {
   return loading ? (
     <LoadingSpinner />
   ) : (
-    <div className="grid grid-cols-2 gap-4">
-      <div>
-        <Swiper
-          slidesPerView={1}
-          navigation
-          pagination={{ clickable: true }}
-          modules={[Navigation, Pagination]}
-          className="esport-swiper"
-        >
-          {product.images.map((image) => (
-            <SwiperSlide key={image}>
-              <img
-                src={image}
-                alt={product.title}
-                style={{ width: "100%", height: "100%", objectFit: "cover" }}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-      <div className="flex flex-col space-y-3">
-        <div className="flex justify-between">
-          <p className="font-semibold text-5xl">{product.title}</p>
-          <div className="flex items-center gap-4">
-            <div className="flex items-center gap-1">
-              {[1, 2, 3, 4, 5].map((starNbr, i) => {
-                if (starNbr <= product.rating) {
-                  return <Star />;
-                }
-                return <EmptyStar />;
-              })}
-              <p>{product.rating}</p>
+    <div className="min-h-screen flex items-center">
+      <div className="grid grid-cols-4 gap-4">
+        <div className="col-span-2">
+          <Swiper
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            modules={[Navigation, Pagination]}
+            className="esport-swiper"
+          >
+            {product.images.map((image) => (
+              <SwiperSlide key={image}>
+                <img
+                  src={image}
+                  alt={product.title}
+                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div className="col-span-2 flex flex-col justify-center space-y-3">
+          <div className="flex justify-between">
+            <p className="font-semibold text-5xl">{product.title}</p>
+            <div className="flex items-center gap-4">
+              <div className="flex items-center gap-1">
+                {[1, 2, 3, 4, 5].map((starNbr, i) => {
+                  if (starNbr <= product.rating) {
+                    return <Star />;
+                  }
+                  return <EmptyStar />;
+                })}
+                <p>{product.rating}</p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <p className="w-10/12 font-medium text-2xl">{product.description}</p>
+          <p className="w-10/12 font-medium text-2xl">{product.description}</p>
 
-        <div className={`${styles.info} space-y-1`}>
-          <p>Stock: {product.stock}</p>
-          <p>Brand: {product.brand}</p>
-          <p>Category: {product.category}</p>
-        </div>
+          <div className={`${styles.info} space-y-1`}>
+            <p>Stock: {product.stock}</p>
+            <p>Brand: {product.brand}</p>
+            <p>Category: {product.category}</p>
+          </div>
 
-        <p className="discount">{product.discountPercentage}%</p>
+          <p className="discount">{product.discountPercentage}%</p>
 
-        <div>
-          <p>{product.price} $</p>
-          <button
-            onClick={() =>
-              toast.info(`You added ${product.title} to your cart!`)
-            }
-          >
-            Add to cart
-          </button>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold text-6xl">{product.price} $</p>
+            <button
+              onClick={() =>
+                toast.info(`You added ${product.title} to your cart!`)
+              }
+              className="font-semibold text-2xl text-white bg-black rounded-full px-8 py-3"
+            >
+              Add to cart
+            </button>
+          </div>
         </div>
       </div>
     </div>
