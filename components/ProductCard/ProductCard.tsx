636c0550a4783e012e40be384b296004bd41eb55
Gergely Kiss <gergely.kiss@r34dy.io>
2022-11-19 11:36:38 +0100

    product card styling + image domain

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
 import Link from "next/link";
 import React, { FC } from "react";
 import { IProductCardData } from "../../types/product";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 import styles from "./ProductCard.module.scss";
 
 type Props = {
@@ -9,7 +11,18 @@ type Props = {
 
 const ProductCard: FC<Props> = ({ productData }) => {
   return (
-    <div>
+    <div className={`${styles["card-container"]}`}>
+      <div className={styles["image-container"]}>
+        <Image
+          src={productData.thumbnail}
+          alt={productData.title}
+          className={styles.image}
+          loading="eager"
+          fill
+          quality={100}
+        />
+        <p className={styles.discount}>{productData.discountPercentage}%</p>
+      </div>
       <h2>{productData.title}</h2>
       <Link href={`/product/${productData.id}`}>See details</Link>
     </div>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.dummyjson.com",
+      },
+    ],
+  },
 };
