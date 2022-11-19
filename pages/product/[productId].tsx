f8c7a4f01a466d464e24aef4b3bd5c7d23cb1f1d
Gergely Kiss <gergely.kiss@r34dy.io>
2022-11-19 10:55:18 +0100

    fetching product data on product page + routing stablize

--- pages/product/[productId].tsx
+++ pages/product/[productId].tsx
@@ -1,16 +1,45 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { getProductById } from "../../axios/product";
+import { IProduct } from "../../types/product";
+import { toast } from "react-toastify";
+import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 
 type Props = {};
 
 const ProductPage: NextPage<Props> = () => {
+  const router = useRouter();
   const { productId } = useRouter().query as { productId: string };
+  const [product, setProduct] = useState<IProduct>();
+  const [loading, setLoading] = useState<boolean>(true);
 
-  // GET PRODUCT
-  useEffect(() => {}, []);
+  const fetchProducts = useCallback(async () => {
+    if (productId) {
+      try {
+        const product = await getProductById(productId);
+        setProduct(product);
+        setLoading(false);
+        toast.success("Products loaded!");
+      } catch (e) {
+        console.log(productId);
+        router.push("/");
+        toast.error("Something went wrong! Please try again!");
+      }
+    }
+  }, [productId]);
 
-  return <div>ProductPage for {productId}</div>;
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  return loading ? (
+    <LoadingSpinner />
+  ) : (
+    <div>
+      <h3>{product?.title}</h3>
+    </div>
+  );
 };
 
 export default ProductPage;
