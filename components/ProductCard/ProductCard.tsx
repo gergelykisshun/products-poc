58842f1abdf32b5e27c934d0d2a2c66ba8f68cba
Gergely Kiss <gergely.kiss@r34dy.io>
2022-11-19 12:09:34 +0100

    basic product card ready

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -10,6 +10,9 @@ type Props = {
 };
 
 const ProductCard: FC<Props> = ({ productData }) => {
+  const productTitleArr = productData.title.split(" ");
+  const productTextArr = productData.description.split(" ");
+
   return (
     <div className={`${styles["card-container"]}`}>
       <div className={styles["image-container"]}>
@@ -23,8 +26,27 @@ const ProductCard: FC<Props> = ({ productData }) => {
         />
         <p className={styles.discount}>{productData.discountPercentage}%</p>
       </div>
-      <h2>{productData.title}</h2>
-      <Link href={`/product/${productData.id}`}>See details</Link>
+      <div className="flex items-center justify-between w-full">
+        <p className="font-semibold text-xl">
+          {productTitleArr.length < 2
+            ? productData.title
+            : productTitleArr.slice(0, 2).join(" ") + "..."}
+        </p>
+        <p className="font-semibold text-2xl">{productData.price} $</p>
+      </div>
+      <div className="w-9/12 self-start">
+        <p>
+          {productTextArr.length < 9
+            ? productData.description
+            : productTextArr.slice(0, 9).join(" ") + "..."}
+        </p>
+      </div>
+      <Link
+        href={`/product/${productData.id}`}
+        className="flex items-center justify-center rounded-3xl bg-black text-white w-full py-2"
+      >
+        See details
+      </Link>
     </div>
   );
 };
